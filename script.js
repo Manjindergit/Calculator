1ce15c6760110d13c689b36b9815e6e8b3be5077
Manjindergit <manjindergrewal1997@gmail.com>
2022-08-27 20:26:39 -0700

    addition operation working

--- script.js
+++ script.js
@@ -1,19 +1,56 @@
-function add(a,b){
-    return a+b;
+let operate = false;
+let operand = '';
+const inputDisplay = document.querySelector('.input');
+const resultDisplay = document.querySelector('.resultOutput');
+
+function add(a, b) {
+    return a + b;
 }
 
-function subtract(a,b){
-    return a-b;
+function subtract(a, b) {
+    return a - b;
 }
 
-function multiply(a,b){
-    return a*b;
+function multiply(a, b) {
+    return a * b;
 }
 
-function division(a,b){
-    return a/b;
+function division(a, b) {
+    return a / b;
 }
 
-function operate(){
+function calculate() {
     
+    switch (operand) {
+        case '+':
+            resultDisplay.textContent=add(parseInt(resultDisplay.textContent),parseInt(inputDisplay.textContent))
+            inputDisplay.textContent=0;
+            
+            break;
+        case '-':
+            // code block
+            break;
+        case '*':
+            // code block
+            break;
+        case '/':
+            // code block
+            break;
+        default:
+        // code block
+    }
 }
+
+function input(number) {
+    if (operate === false) {
+        resultDisplay.textContent = number;
+    }
+    
+    inputDisplay.textContent = number;
+}
+
+function operator(operandSign) {
+    operate = true;
+    operand=operandSign;
+}
+
